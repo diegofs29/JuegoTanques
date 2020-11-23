d89ddf4eebfd2e513d4ee237b2fbfee592bfb041
diegofs29 <diegofdezsrz12@gmail.com>
2020-11-23 17:44:51 +0100

    Cambios en el jugador

--- JuegoLibreTanques/Player.cpp
+++ JuegoLibreTanques/Player.cpp
@@ -1,6 +1,6 @@
 #include "Player.h"
 
 Player::Player(float x, float y, Game* game)
-	: Actor("res/playerIdle.png", x, y, 128, 128, game) {
+	: Actor("res/playerIdle.png", x, y, 72, 80, game) {
 
 }
